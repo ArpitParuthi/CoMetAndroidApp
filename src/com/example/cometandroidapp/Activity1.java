0275f58779813798c5cc4fa19f5a84ccce32defd
Arpit Paruthi <arpitparuthi@hotmail.com>
2014-12-07 20:33:37 -0500

    Cleaning up

--- src/com/example/cometandroidapp/Activity1.java
+++ src/com/example/cometandroidapp/Activity1.java
@@ -1,21 +1,13 @@
 package com.example.cometandroidapp;
 
-
 import java.io.BufferedReader;
 import java.io.ByteArrayOutputStream;
-import java.io.File;
-import java.io.FileOutputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.net.URLConnection;
 import java.net.URLEncoder;
-
-import org.json.JSONArray;
 import org.json.JSONObject;
-import org.json.JSONTokener;
-
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.Context;
@@ -34,27 +26,22 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.*;
 
-
 public class Activity1 extends Activity  {
+	
 	ImageView iv;
 	Bitmap bm;
+	
 	private boolean isNetworkAvailable() {
 	    ConnectivityManager connectivityManager 
 	          = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
 	    NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
 	    return activeNetworkInfo != null && activeNetworkInfo.isConnected();
 	}
-	
-	@SuppressWarnings("deprecation")
+
 	public void showAlertDialog(Context context, String title, String message, Boolean status) {
         AlertDialog alertDialog = new AlertDialog.Builder(context).create();
-
-        // Setting Dialog Title
         alertDialog.setTitle(title);
-
-        // Setting Dialog Message
         alertDialog.setMessage(message);
-        // Setting OK Button
         alertDialog.setButton("OK", new DialogInterface.OnClickListener() {
             public void onClick(DialogInterface dialog, int which) {
              finish();
@@ -62,63 +49,49 @@ public class Activity1 extends Activity  {
         });
         alertDialog.show();
 	}
-	@Override
+
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		
 		if(!isNetworkAvailable()){
 		    showAlertDialog(Activity1.this, "No Internet Connection!",
-                    "You don't have internet connection. Please switch on your internet connection and try again.", false);
+                    "You don't have internet connection. Please switch on the internet connection and try again.", false);
 		} else {
-		setContentView(R.layout.activity1);
-        iv = (ImageView)findViewById(R.id.imageView1);
-		Button b = (Button) findViewById(R.id.take);
-		b.setOnClickListener(new OnClickListener() {
-		
-			@Override
-			public void onClick(View v) {
-				// TODO Auto-generated method stub
-				Intent intent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
-				startActivityForResult(intent,0);	
-			}
-		});
-		Button b2 = (Button) findViewById(R.id.next);
-        b2.setVisibility(View.GONE);
-		b2.setOnClickListener (new OnClickListener() {
-			
-			@Override
-			public void onClick(View v) {
-				// TODO Auto-generated method stub
-				Intent intent = new Intent(Activity1.this,Activity2.class);
-				intent.putExtra("image",bm);
-				startActivity(intent);	
-			}
-		});
+			setContentView(R.layout.activity1);
+			iv = (ImageView)findViewById(R.id.imageView1);
+			Button b = (Button) findViewById(R.id.take);
+			b.setOnClickListener(new OnClickListener() {
+				public void onClick(View v) {
+					Intent intent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+					startActivityForResult(intent,0);	
+				}
+			});
+			Button b2 = (Button) findViewById(R.id.next);
+			b2.setVisibility(View.GONE);
+			b2.setOnClickListener (new OnClickListener() {
+				public void onClick(View v) {
+					Intent intent = new Intent(Activity1.this,Activity2.class);
+					intent.putExtra("image",bm);
+					startActivity(intent);	
+				}
+			});
 		} 
 	}
 
-	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
 
-	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
 		int id = item.getItemId();
 		if (id == R.id.action_settings) {
 			return true;
 		}
 		return super.onOptionsItemSelected(item);
 	}
-	
-	@Override
+
 	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-		// TODO Auto-generated method stub
-		
 		if(resultCode == RESULT_OK) {
 			super.onActivityResult(requestCode, resultCode, data);
 			bm = (Bitmap) data.getExtras().get("data");
@@ -135,8 +108,7 @@ public class Activity1 extends Activity  {
 }
 
 class Imgur extends AsyncTask<String, Void, String> {
-	
-	@Override
+
 	protected String doInBackground(String... params) {
 		String Image = params[0];
 		String key = "8ca9d84ac6bcf3a";
@@ -172,7 +144,6 @@ class Imgur extends AsyncTask<String, Void, String> {
 			e.printStackTrace();
 		}
 		return null;
-	} 
-	
+	} 	
 }
 	
--- src/com/example/cometandroidapp/Activity3.java
+++ src/com/example/cometandroidapp/Activity3.java
@@ -1,7 +1,6 @@
 package com.example.cometandroidapp;
 
 import java.util.Calendar;
-
 import android.os.Bundle;
 import android.app.Activity;
 import android.app.DatePickerDialog;
@@ -24,12 +23,10 @@ public class Activity3 extends Activity{
 	ImageView iv1;
 	TimePickerFragment tpf;
 	EditText et1, et2, et3, et4;
-	
-	@Override
+
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity3);
-		
 		Intent intent = getIntent();
 		bm = intent.getParcelableExtra("image");
 		iv1 = (ImageView)findViewById(R.id.iv1);
@@ -52,6 +49,7 @@ public class Activity3 extends Activity{
 	    newFragment.setArguments(bundle);
 	    newFragment.show(getFragmentManager(), "timePicker");
 	}
+	
 	public void showTimePickerDialog2(View v) {
 		Bundle bundle = new Bundle();
 	    DialogFragment newFragment = new TimePickerFragment();
@@ -59,50 +57,44 @@ public class Activity3 extends Activity{
 	    newFragment.setArguments(bundle);
 	    newFragment.show(getFragmentManager(), "timePicker");
 	} 
-	
-	@Override
+
 	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.activity3, menu);
 		return true;
 	}
+	
 	class TimePickerFragment extends DialogFragment implements TimePickerDialog.OnTimeSetListener {
+		
 		String x;
+		
 		public Dialog onCreateDialog(Bundle savedInstanceState) {
 					Bundle bundle = this.getArguments();
 					final Calendar c = Calendar.getInstance();
 					int hour = c.get(Calendar.HOUR_OF_DAY);
 					int minute = c.get(Calendar.MINUTE);
 					 x= bundle.getString("id");
-					return new TimePickerDialog(getActivity(), this, hour, minute,DateFormat.is24HourFormat(getActivity()));
-					
+					return new TimePickerDialog(getActivity(), this, hour, minute,DateFormat.is24HourFormat(getActivity()));		
 		}
-			@Override
-			public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+	
+		public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
 				if(x=="1")
 					et3.setText(hourOfDay+": "+minute);
 				else if (x=="2")
-					et4.setText(hourOfDay+": "+minute);
-				
+					et4.setText(hourOfDay+": "+minute);	
 			}
-			
-		}
+	}
+	
 	class DatePickerFragment extends DialogFragment implements DatePickerDialog.OnDateSetListener {
 
-		@Override
 		public Dialog onCreateDialog(Bundle savedInstanceState) {
-			// Use the current date as the default date in the picker
 			final Calendar c = Calendar.getInstance();
 			int year = c.get(Calendar.YEAR);
 			int month = c.get(Calendar.MONTH);
 			int day = c.get(Calendar.DAY_OF_MONTH);
-
-			// Create a new instance of DatePickerDialog and return it
 			return new DatePickerDialog(getActivity(), this, year, month, day);
 		}
 
 		public void onDateSet(DatePicker view, int year, int month, int day) {
-			// Do something with the date chosen by the user
 			et2.setText((month+1)+"/"+day+"/"+year);
 		}
 	}
